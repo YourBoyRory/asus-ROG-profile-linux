f6f386993142e2b539a6a9ded6c4ab270a6cdcc2
Rory <yourboyrory@gmail.com>
2023-09-29 16:01:10 -0400

    made uninstaller

--- install.sh
+++ install.sh
@@ -3,4 +3,5 @@
 sudo mkdir /opt/asus-ROG-profile-linux/
 sudo mkdir /opt/asus-ROG-profile-linux/assets
 sudo cp ./assets/*.png /opt/asus-ROG-profile-linux/assets
-sudo cp ./asusfan.sh /usr/local/bin/asusfan.sh
+sudo cp ./asusfan.sh /usr/local/bin/asusfan
+sudo chmod 755 /usr/local/bin/asusfan
---
+++ uninstall.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sudo rm -r /opt/asus-ROG-profile-linux/
+sudo rm /usr/local/bin/asusfan
