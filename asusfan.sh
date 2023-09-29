992de4608ba867b1a894f144a4b43397076f6932
Rory <yourboyrory@gmail.com>
2023-09-29 18:56:36 -0400

    added back no notify and fixed display missfire bug

--- asusfan.sh
+++ asusfan.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 echo 0 > /tmp/notifyCount
 
 notify() {
@@ -47,11 +46,14 @@ getCenter() {
 }
 
 if [[ $1 == "--quiet" || $2 == "--quiet" ]]; then
-	asusctl profile -PQuiet && notify-send -pe "$(asusctl profile -p)" "Default fan mode loaded." > /tmp/notifyID
+	asusctl profile -PQuiet && notify-send --icon="power-profile-power-saver-symbolic" -pe "Forced: $(asusctl profile -p)" "Default fan mode loaded." > /tmp/notifyID
+    lastMode="Quiet"
 elif [[ $1 == "--balanced" || $2 == "--balanced" ]]; then
-	asusctl profile -PBalanced && notify-send -pe "$(asusctl profile -p)" "Default fan mode loaded." > /tmp/notifyID
+	asusctl profile -PBalanced && notify-send --icon="power-profile-balanced-symbolic" -pe "Forced: $(asusctl profile -p)" "Default fan mode loaded." > /tmp/notifyID
+    lastMode="Balanced"
 elif [[ $1 == "--performance" || $2 == "--performance" ]]; then
-	asusctl profile -PPerformance && notify-send -pe "$(asusctl profile -p)" "Default fan mode loaded." > /tmp/notifyID
+	asusctl profile -PPerformance && notify-send --icon="power-profile-performance-symbolic" -pe "Forced: $(asusctl profile -p)" "Default fan mode loaded." > /tmp/notifyID
+    lastMode="Performance"
 elif [[ $1 == "-h" ]]; then
 	echo "    "
 	echo " Fixes the fan selector button on Asus laptops using asusctl."
@@ -63,7 +65,9 @@ elif [[ $1 == "-h" ]]; then
 	echo "    "
 	echo "    "
 	echo "    -h                Help."
-	echo "    -n  --notify      display old notifications when switching"
+	echo "    -n  --no-notify   Don't display notifications when switching, its more responsive and uses less resources"
+	echo "    -s  --splash      display splash when switching, similar to the ones used in windows. Not super responsive"
+    echo "                      implies --no-notify."
 	echo "    "
 	echo "    --quiet	       Starts the script with setting the fan mode to Quiet by default."
 	echo "    --balanced       Starts the script with setting the fan mode to Balanced by default."
@@ -72,35 +76,77 @@ elif [[ $1 == "-h" ]]; then
 	exit
 else
 	notify-send -pe "$(asusctl profile -p)" "Fan mode changer listening..."> /tmp/notifyID
+    lastMode=$(asusctl profile -p | awk '{print $NF}')
 fi
 
-if [[ $1 == "--notify" || $2 == "--notify" || $1 == "-n" || $2 == "-n" ]]; then
-	while true; do
-		if [[ $(acpi_listen -c 1) = " 0B3CBB35-E3C2- 000000ff 00000000" ]]; then
-			if [[ $(powerprofilesctl list | grep '*') = "* power-saver:" ]]; then
-				asusctl profile -PQuiet && notify-send --urgency=critical -rp $(head /tmp/notifyID) "$(asusctl profile -p)" "Fan mode changed to Quiet." > /tmp/notifyID
-				notify $?
-			elif [[ $(powerprofilesctl list | grep '*') = "* balanced:" ]]; then
-				asusctl profile -PBalanced && notify-send --urgency=critical -rp $(head /tmp/notifyID) "$(asusctl profile -p)" "Fan mode changed to Balanced." > /tmp/notifyID
-				notify $?
-			elif [[ $(powerprofilesctl list | grep '*') = "* performance:" ]]; then
-				asusctl profile -PPerformance && notify-send --urgency=critical -rp $(head /tmp/notifyID) "$(asusctl profile -p)" "Fan mode changed to Performance." > /tmp/notifyID
-				notify $?
-			fi
-		fi
-	done
-else 
+
+if [[ $1 == "--splash" || $2 == "--splash" || $1 == "-s" || $2 == "-s" ]]; then
+    splash $lastMode
     while true; do
 		if [[ $(acpi_listen -c 1) = " 0B3CBB35-E3C2- 000000ff 00000000" ]]; then
 			if [[ $(powerprofilesctl list | grep '*') = "* power-saver:" ]]; then
-				asusctl profile -PQuiet
-                splash 0
+                if [[ "$lastMode" != "Quiet" ]]; then
+                    asusctl profile -PQuiet
+                    lastMode="Quiet"
+                    splash $lastMode
+                fi
 			elif [[ $(powerprofilesctl list | grep '*') = "* balanced:" ]]; then
-				asusctl profile -PBalanced
-                splash 1
+                if [[ "$lastMode" != "Balanced" ]]; then
+                    asusctl profile -PBalanced
+                    lastMode="Balanced"
+                    splash $lastMode
+                fi
 			elif [[ $(powerprofilesctl list | grep '*') = "* performance:" ]]; then
-				asusctl profile -PPerformance
-                splash 2
+                if [[ "$lastMode" != "Performance" ]]; then
+                    asusctl profile -PPerformance
+                    lastMode="Performance"
+                    splash $lastMode
+                fi
+			fi
+		fi
+	done
+elif [[ $1 == "--no-notify" || $2 == "--no-notify" || $1 == "-n" || $2 == "-n" ]]; then
+	while true; do
+		if [[ $(acpi_listen -c 1) = " 0B3CBB35-E3C2- 000000ff 00000000" ]]; then
+			if [[ $(powerprofilesctl list | grep '*') = "* power-saver:" ]]; then
+                if [[ "$lastMode" != "Quiet" ]]; then
+                    asusctl profile -PQuiet
+                    lastMode="Quiet"
+                fi
+			elif [[ $(powerprofilesctl list | grep '*') = "* balanced:" ]]; then
+				if [[ "$lastMode" != "Balanced" ]]; then
+                    asusctl profile -PBalanced
+                    lastMode="Balanced"
+                fi
+			elif [[ $(powerprofilesctl list | grep '*') = "* performance:" ]]; then
+				if [[ "$lastMode" != "Performance" ]]; then
+                    asusctl profile -PPerformance
+                    lastMode="Performance"
+                fi
+			fi
+		fi
+	done
+else
+	while true; do
+		if [[ $(acpi_listen -c 1) = " 0B3CBB35-E3C2- 000000ff 00000000" ]]; then
+			if [[ $(powerprofilesctl list | grep '*') = "* power-saver:" ]]; then
+                if [[ "$lastMode" != "Quiet" ]]; then
+                    asusctl profile -PQuiet && notify-send --icon="power-profile-power-saver-symbolic" --urgency=critical -rp $(head /tmp/notifyID) "$(asusctl profile -p)" "The CPU will under-clock to use less power and make less noise." > /tmp/notifyID
+                    notify $?
+                    lastMode="Quiet"
+                fi
+			elif [[ $(powerprofilesctl list | grep '*') = "* balanced:" ]]; then
+				if [[ "$lastMode" != "Balanced" ]]; then
+                    asusctl profile -PBalanced && notify-send --icon="power-profile-balanced-symbolic" --urgency=critical -rp $(head /tmp/notifyID) "$(asusctl profile -p)" "This CPU will run at base clock to balance battery and performance, fan may ramp up under load." > /tmp/notifyID
+                    notify $?
+                    lastMode="Balanced"
+                fi
+			elif [[ $(powerprofilesctl list | grep '*') = "* performance:" ]]; then
+				if [[ "$lastMode" != "Performance" ]]; then
+                    asusctl profile -PPerformance && notify-send --icon="power-profile-performance-symbolic" --urgency=critical -rp $(head /tmp/notifyID) "$(asusctl profile -p)" "The CPU will be allowed to boost-clock to ensure the best performance, fan will ramp up under small loads and uses more battery." > /tmp/notifyID
+                    notify $?
+                    lastMode="Performance"
+                fi
 			fi
 		fi
 	done
